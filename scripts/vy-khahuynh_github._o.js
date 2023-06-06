2dd3ff5e38b72ee1b6dd01d26daa1726917814c6
Vy-KhaHuynh <vy-kha.huynh@mail.mcgill.ca>
2023-06-05 22:48:18 -0400

    Added intro animation

--- scripts/vy-khahuynh_github._o.js
+++ scripts/vy-khahuynh_github._o.js
@@ -35,7 +35,47 @@ function activateSuperlaser(){
 }
 
 addEventListener("load", addStars);
+addEventListener("load", introloop);
 
-window.addEventListener('scroll', () => {
-  document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
+function scrolltop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+let names = ["Vy-Kha Huynh", "a Software Engineer", "a Blackhole fanatic", "a LEGO enthusiast"];
+
+function introloop(){
+  var i = 1;
+  
+  intro(0);
+  
+  setInterval(function(){
+    intro(i);
+    i = (i + 1) % names.length;
+  }, 15000)
+}
+
+function intro(i){
+  var cur = document.querySelectorAll('#switch')[0];
+  cur.classList.remove('active');
+  cur.innerHTML = '';
+  for(var j = 0 ; j < names[i].length ; j++){
+    var letter = document.createElement('span');
+    letter.innerHTML = names[i][j];
+    cur.appendChild(letter);
+  }
+  cur.classList.add('active');
+}
+
+// let options = {
+//   root: null,
+//   rootMargin: "0px",
+//   threshold: 0.3
+// }
+
+// let callback = (entries) => {
+//   entries.forEach((entry) =>{
+//   });
+// };
+
+// const observer = new IntersectionObserver(callback, options);
+
